ef8b28fbd9d3f15d218aea40ce9968cae7b35f7b
ldps21 <luisd2197@gmail.com>
2021-12-19 15:06:30 +0000

    Navbar ready

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import styles from './Navbar.module.css'
+import React, { useState, useEffect } from "react";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <label>Marisela Nail's</label>
-            <ul className={styles.menu}>
-                <li className={styles.menuItem}>home</li>
-                <li className={styles.menuItem}>serviços</li>
-                <li className={styles.menuItem}>contactos</li>
-            </ul>
-            <button type="button" className={styles.btn}>BTN</button>
-        </nav>
-    )
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+  const toggleNav = () => {
+      setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+      const changeWidth = () => {
+        setScreenWidth(window.innerWidth);
+      }
+      window.addEventListener('resize', changeWidth)
+
+      return () => {
+        window.removeEventListener('resize', changeWidth)
+      }
+
+  }, [])  
+
+  return (
+    <nav>
+      <label>Marisela Nail's</label>
+      {(toggleMenu || screenWidth > 500) && (
+        <ul className={styles.menu}>
+          <li className={styles.menuItem}>home</li>
+          <li className={styles.menuItem}>serviços</li>
+          <li className={styles.menuItem}>contactos</li>
+        </ul>
+      )}
+
+      <button onClick={toggleNav} type="button" className={styles.btn}>
+        BTN
+      </button>
+    </nav>
+  );
 }
